6c416fa811e1ba959804850ca3c83e943596e649
elbahjacharafeddine <101811374+elbahjacharafeddine@users.noreply.github.com>
2023-09-04 14:28:14 +0100

    update ./.

--- src/views/layout/Notifications.js
+++ src/views/layout/Notifications.js
@@ -103,7 +103,7 @@ const Notifications = () => {
 
         // const response = await Axios.get('http://localhost:2000/prof/scopus/'+followedUser.authorId)
         // const response = await Axios.get('https://rs-scraper-master.onrender.com/prof/scopus/' + followedUser.authorId)
-        const response = await Axios.get('http://ms2dpddval.ap.loclx.io/prof/scopus/' + followedUser.authorId)
+        const response = await Axios.get('https://bxccvni2jx.loclx.io/prof/scopus/' + followedUser.authorId)
         // console.log("");
         // console.log(response.data);
 
